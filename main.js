07ae76aa1d297df85e2489f6c0274a784c1cc48f
Terrax <terrax@abv.bg>
2019-01-13 22:29:15 +0200

    Creating base structure, Add User and Show All Users functionality.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var users = [];
 
-$('#addUserBtn').on('click', event, function() {
-    event.preventDefault();
+$('#addUserBtn').on('click', function(e) {
+    e.preventDefault();
     let id = $('#userID').val();
     let email = $('#userEmail').val();
     let firstName = $('#givenName').val();
@@ -12,17 +12,33 @@ $('#addUserBtn').on('click', event, function() {
     $('#addUser')[0].reset();
 });
 
-$('#showAllUsers').on('click', event, function () {
-    event.preventDefault();
+$('#showAllUsers').on('click', function (e) {
+    e.preventDefault();
     $('#allUsers').show();
+    let editBtn = "<button class=\"edit-button\" type=\"submit\">Edit</button>";
+    let deleteBtn = "<button class=\"delete-button\" type=\"submit\">Delete</button>";
 
-    for (var i = 0; i < users.length; i++) {
-        var userData = users[i];
-        var userDataRow = "<tr><td>" + userData.id + "</td><td>" + userData.email + "</td><td>" +
-            userData.firstName + "</td><td>" + userData.lastName + "</td><td>" + userData.created + "</td></tr>";
+    for (let i = 0; i < users.length; i++) {
+        let userData = users[i];
+        let userDataRow = "<tr><td class='table-row-user-id'>" + userData.id + "</td><td>" + userData.email + "</td><td>" +
+            userData.firstName + "</td><td>" + userData.lastName + "</td><td>" + userData.created +
+            editBtn + deleteBtn + "</td></tr>";
 
         $('#allUsersData').append(userDataRow);
     }
+});
+
+$('table').on('click', 'tr button.delete-button', function(e) {
+    e.preventDefault();
+    let userID = $(this).closest('tr').find('.table-row-user-id').text();
+    //$(this).closest('tr').remove();
+
+    deleteUser(userID);
+});
+
+$('table').on('click', 'tr button.edit-button', function(e) {
+    e.preventDefault();
+    console.log('edit button clicked');
 });
 
 function createUser(id, email, firstName, lastName, created) {
@@ -35,5 +51,15 @@ function createUser(id, email, firstName, lastName, created) {
     };
 
     users.push(user);
+}
+
+function deleteUser(userID) {
+    console.log(users);
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === userID) {
+            users.splice(i, 1);
+            break;
+        }
+    }
     console.log(users);
 }
